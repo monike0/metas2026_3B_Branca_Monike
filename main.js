9033415da0bb6622177f9c4de322a66df1f4aef1
monike0 <marques.monike@escola.pr.gov.br>
2026-07-06 11:53:10 -0300

    bdg

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const textos = document.querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function () {
         for (let j = 0; j <botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
+            botoes[i].classList.remove("ativo");
+            textos[i].classList.remove("ativo");
         }
          botoes[j].classList.add("ativo");
         textos[j].classList.add("ativo");
